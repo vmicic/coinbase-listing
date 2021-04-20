0d3225d1c36d2d9561ee80ad79f837f9e7151895
vmicic <vukasin.micic5@gmail.com>
2021-04-20 10:55:32 +0200

    Add support for multiple new titles

--- src/handler.js
+++ src/handler.js
@@ -9,22 +9,37 @@ const AWS = require('aws-sdk');
 const client = new AWS.DynamoDB.DocumentClient({ region: 'us-east-1' });
 
 const POSTS_API_URI = 'https://medium.com/_/api/collections/c114225aeaf7/stream';
+// prettier-ignore
 const DISCORD_WEBHOOK = 'https://discord.com/api/webhooks/833780553488400404/vA6mQgmHhJuXIiW6WTb8PxA-No9edieuuoB4bGbzOPeAEu7Qj5Nb-OqIyZsU-j2f2UeO';
 
 const { DATA_TABLE } = process.env;
 
-const getTitlesFromApi = async (url) => {
-  const response = await axios.get(url);
-  const body = JSON.parse(response.data.substring(16));
+const getTitlesFromBody = (body) => {
   const posts = { ...body.payload.references.Post };
   const postsId = Object.keys(posts);
   return postsId.map((id) => posts[id].title);
 };
 
+const getTitlesFromApi = async (url) => {
+  const response = await axios.get(url);
+  const body = JSON.parse(response.data.substring(16));
+  const titles = getTitlesFromBody(body);
+
+  const { to, page } = body.payload.paging.next;
+  const POSTS_API_URI_NEXT = `${POSTS_API_URI}?to=${to}&page=${page}`;
+  const responseNext = await axios.get(POSTS_API_URI_NEXT);
+  const bodyNext = JSON.parse(responseNext.data.substring(16));
+  const titlesNext = getTitlesFromBody(bodyNext);
+
+  return titles.concat(titlesNext);
+};
+
 const getSavedTitlesFromDatabase = async () => {
-  const data = await client.scan({
-    TableName: DATA_TABLE,
-  }).promise();
+  const data = await client
+    .scan({
+      TableName: DATA_TABLE,
+    })
+    .promise();
   return data.Items.map((item) => item.id);
 };
 
@@ -32,7 +47,12 @@ const saveTitlesToDatabase = async (titles) => {
   const promises = [];
   // replace with bulk write later
   titles.forEach((title) => {
-    const saveTitlePromise = client.put({ TableName: DATA_TABLE, Item: { id: title } }).promise();
+    const saveTitlePromise = client
+      .put({
+        TableName: DATA_TABLE,
+        Item: { id: title },
+      })
+      .promise();
     promises.push(saveTitlePromise);
   });
   await Promise.all(promises);
@@ -41,17 +61,16 @@ const saveTitlesToDatabase = async (titles) => {
 // note, I've used the sort inside
 // also, this is a pretty bad way of handling this
 // but will be updated
-const compareTitles = (oldTitles, titlesFromApi) =>
-  JSON.stringify(oldTitles.sort()) !== JSON.stringify(titlesFromApi.sort());
-
+const getNewTitles = (oldTitles, titles) => titles.filter((title) => !oldTitles.includes(title));
 // just to make sure that the case doesn't mess us up
-const isTitleLaunching = (title) => title.toLowerCase().includes('launching on Coinbase'.toLowerCase());
+const isTitleLaunching = (title) =>
+  title.toLowerCase().includes('launching on Coinbase'.toLowerCase());
 
 const getLaunchingCoins = (title) => {
   const words = title.split(' ');
   const coins = words
     .filter((word) => {
-      // get only words in between parentheses
+      // get only words with parantheses from both sides
       const regExp = /\(([^)]+)\)/;
       return regExp.exec(word);
     })
@@ -68,18 +87,22 @@ const getLaunchingCoins = (title) => {
 const detectListing = async () => {
   const titles = await getTitlesFromApi(POSTS_API_URI);
   const oldTitles = await getSavedTitlesFromDatabase();
+
+  const newTitles = getNewTitles([...oldTitles], [...titles]);
+  if (newTitles.length === 0) {
+    return [];
+  }
+
+  // save only if there are new titles
   await saveTitlesToDatabase(titles);
 
-  const isNewTitleAdded = compareTitles(oldTitles, titles);
-  if (!isNewTitleAdded) {
-    return [];
-  }
+  let coins = [];
+  newTitles.forEach((title) => {
+    if (isTitleLaunching(title)) {
+      coins = coins.concat(getLaunchingCoins(title));
+    }
+  });
 
-  const newTitle = titles[0];
-  if (!isTitleLaunching(newTitle)) {
-    return [];
-  }
-  const coins = getLaunchingCoins(newTitle);
   return coins;
 };
 
@@ -89,7 +112,9 @@ const alertLaunching = async (coins) => {
     avatar: '',
     username: 'Coinbase listing bot',
   };
-  await axios.post(DISCORD_WEBHOOK, JSON.stringify(data), { headers: { 'Content-type': 'application/json' } });
+  await axios.post(DISCORD_WEBHOOK, JSON.stringify(data), {
+    headers: { 'Content-type': 'application/json' },
+  });
 };
 
 const main = async () => {
